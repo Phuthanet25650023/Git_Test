e26c5e483e951a5dd923c10b768b215f61178330
Phuthanet25650023 <phuthanet@mut.ac.th>
2023-07-13 14:12:59 +0700

    index 130766_5

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 echo "CPE 6211850006";
-#git config --global user.email "phuthanet@mut.ac.th"
-#git config --global user.name "Phuthanet25650023"
+#git config --global user.name Phuthanet25650023
+#git config --global user.email phuthanet@mut.ac.th
+
+#git add index.php
+#git status
+
+#git commit -m "index 130766"
+#git log
+
+#git clean -n
+#git clean -df
+
+#git reset --hard 5015bcdd75fad0170d0ed353c80ae4fc57b911be
 ?>
+<?php echo "Mahanakorn ";?>
+<?php echo "Computer Engineering ";?>
+<?php echo "Computer Engineering01 ";?>
+<?php echo "Computer Engineering02 ";?>
