0fe46a8cb944c58b71ca790d88ad9d1948e76d13
Phuthanet25650023 <phuthanet@mut.ac.th>
2023-07-13 14:26:16 +0700

    index Command Git

--- index.php
+++ index.php
@@ -3,16 +3,21 @@ echo "CPE 6211850006";
 #git config --global user.name Phuthanet25650023
 #git config --global user.email phuthanet@mut.ac.th
 
-#git add index.php
+#git add index.php 
 #git status
 
-#git commit -m "index 130766"
+#git commit -m "index 130766" ข้อความที่ใช้สำหรับเเจ้งการ Update
 #git log
 
 #git clean -n
 #git clean -df
 
 #git reset --hard 5015bcdd75fad0170d0ed353c80ae4fc57b911be
+
+
+#git remote add origin https://github.com/Phuthanet25650023/Git_Test.git
+#git remote -v
+
 ?>
 <?php echo "Mahanakorn ";?>
 <?php echo "Computer Engineering ";?>
