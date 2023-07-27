fed943dad0b6a3d09ccb2d46d0398eb43f003301
Phuthanet25650023 <phuthanet@mut.ac.th>
2023-07-27 13:20:07 +0700

    index 130766

---
+++ TEST.js
@@ -0,0 +1,34 @@
+/*let user = {name: "Metin",age: 28};
+let x =2;
+
+console.log(x)
+function TEST(a,b)
+{
+    return a+b;
+}
+function hello()
+{
+console.log("Hello World!");
+console.log("Phuthanet 6211850006")
+console.log("5+5 = "+TEST(5,5)+" True "+true+1)
+}
+
+console.log(user);
+console.log([10, 20, 30, 40, 50]);
+console.log(hello());
+
+hello()
+
+
+let Name = "Metin";
+let height = 179.2;
+console.log("%s is %f inches height", Name, height);
+console.log("%d + %d = %d", 3, 5, 3.5 + 5.2);*/
+
+let user = {
+    name: "Metin",
+    safary: 50000,
+    single: true
+    };
+    console.log("Inspect object (%o)", user);
+    console.log("Inspect object (", user, ")");
