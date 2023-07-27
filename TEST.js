eb8a0443e8b1408215ff313a3d52b0b366672de7
Phuthanet25650023 <phuthanet@mut.ac.th>
2023-07-27 14:02:50 +0700

    index 130766

--- TEST.js
+++ TEST.js
@@ -23,12 +23,17 @@ hello()
 let Name = "Metin";
 let height = 179.2;
 console.log("%s is %f inches height", Name, height);
-console.log("%d + %d = %d", 3, 5, 3.5 + 5.2);*/
+console.log("%d + %d = %d", 3, 5, 3.5 + 5.2);
+*/
 
-let user = {
-    name: "Metin",
-    safary: 50000,
-    single: true
-    };
-    console.log("Inspect object (%o)", user);
-    console.log("Inspect object (", user, ")");
+let x = 61;//Gread
+let m = 4, y = 2023;
+console.log(x > 100 ? "Error" : (x >= 71 ? "Gread A" : (x >= 61 ? "Gread B" : (x >= 51 ? "Gread C" : (x >= 41 ? "Gread D" : (x < 41 && x >= 0 ? "Gread F" : (x < 0 ? "Error" : "")))))))
+
+if (m == 4 || m == 6 || m == 9 || m == 11) for (i = 1; i <= 30; i++)console.log("Day =" + i);
+else if (m == 2) 
+{
+    if (y % m == 1) for (i = 1; i <= 28; i++)console.log("Day =" + i);
+    else for (i = 1; i <= 29; i++)console.log("Day =" + i);
+}
+else for (i = 1; i <= 31; i++)console.log("Day =" + i);
