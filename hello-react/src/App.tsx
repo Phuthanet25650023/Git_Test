7c1960d69d8f5754e941b68f65a4c40081f51c41
Phuthanet25650023 <phuthanet@mut.ac.th>
2023-08-24 13:20:24 +0700

    Update

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
@@ -19,14 +19,28 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          Click Me {count}
+        </button>
+      
+        <p>
+          Phuthanet Mekpayom 6211850006
+        </p>
+        <button onClick={() => setCount((count) => count + 50)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      ที่อยู่: 140 ถนน เชื่อมสัมพันธ์ แขวงกระทุ่มราย เขตหนองจอก กรุงเทพมหานคร 10530 ไทย
+      เวลาทำการ: 
+      เปิด ⋅ ปิด 17:00 น.
+      โทรศัพท์: +66 2 988 3666
+      ก่อตั้ง: 27 กุมภาพันธ์ 2533
+      ผู้ก่อตั้ง: สิทธิชัย โภไคยอุดม
+      คติพจน์: ความรู้คือพลัง
+      ชื่อย่อ: มทม. / MUT
+      ที่ตั้ง: 140 ถนนเชื่อมสัมพันธ์ แขวงกระทุ่มราย เขตหนองจอก กรุงเทพมหานคร 10530
+      นายกสภาฯ: ศาสตราจารย์ ดร.สิทธิชัย โภไคยอุดม
+      สถาปนา: 27 กุมภาพันธ์ พ.ศ. 2533 (33 ปี)
       </p>
     </>
   )
